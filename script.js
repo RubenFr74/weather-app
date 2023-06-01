07e188d3fc00eea040d0d09f530e26d710c53f5d
Ruben Franco <ruben.afranco@yahoo.com>
2023-06-01 16:01:28 -0700

    added selectors

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ $(document).ready(function () {
 
     // OpenWeather API
     const apiKey = 'e2a7410d7490dbe73a252ce85b1f3417';
+
+     // Selectors for HTML elements to display weather information
+     const cityEl = $('h2#city');
+     const dateEl = $('h3#date');
+     const weatherIconEl = $('img#weather-icon');
+     const temperatureEl = $('span#temperature');
+     const humidityEl = $('span#humidity');
+     const windEl = $('span#wind');
+     const uvIndexEl = $('span#uv-index');
+     const cityListEl = $('div.cityList');
+     
+     
+});
