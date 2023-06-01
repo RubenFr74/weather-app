a7cf3892a2e246efb4da0f8fd9de6237373c1f7c
Ruben Franco <ruben.afranco@yahoo.com>
2023-06-01 16:02:27 -0700

    started to store past searches

--- script.js
+++ script.js
@@ -13,5 +13,24 @@ $(document).ready(function () {
      const uvIndexEl = $('span#uv-index');
      const cityListEl = $('div.cityList');
      
-     
+    // Selectors for form elements
+   const cityInput = $('#city-input');
+
+   // Store past searched cities
+   let pastCities = [];
+
+   // Helper function to sort cities from https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+   function compare(a, b) {
+       // Use toUpperCase() to ignore character casing
+       const cityA = a.city.toUpperCase();
+       const cityB = b.city.toUpperCase();
+
+       let comparison = 0;
+       if (cityA > cityB) {
+           comparison = 1;
+       } else if (cityA < cityB) {
+           comparison = -1;
+       }
+       return comparison;
+   }
 });
